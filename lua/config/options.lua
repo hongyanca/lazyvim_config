ccddf20dbc453fb788f4e53cb83bb0778916c443
Hong Yan <contact@hongyan.ca>
2024-12-13 01:46:42 -0700

    feat: initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,22 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local is_tmux_session = vim.env.TERM_PROGRAM == "tmux"
+if vim.env.SSH_TTY and not is_tmux_session then
+  local function paste()
+    return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+  end
+  local osc52 = require("vim.ui.clipboard.osc52")
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    -- "EdenEast/nightfox.nvim",
+    -- "folke/tokyonight.nvim",
+    priority = 1000,
+    opts = {},
+    lazy = false,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+      -- colorscheme = "carbonfox",
+      -- colorscheme = "tokyonight-moon",
+    },
+  },
+}
