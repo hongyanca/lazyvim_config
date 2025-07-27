6a5235d3de1ac7f80de10da10202958c178023a9
Hong Yan <contact@yanhong.ca>
2025-07-27 00:15:47 -0600

    feat: update plugins on 2025-07-27

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
 
       vim.keymap.set("n", "<leader>ha", function()
         harpoon:list():add()
-      end, { desc = "Add current file to 󰒤 " })
+      end, { desc = "Add current file to 󱡀 " })
 
       vim.keymap.set("n", "<C-e>", function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
@@ -18,23 +18,27 @@ return {
 
       vim.keymap.set("n", "<leader>hr", function()
         harpoon:list():remove()
-      end, { desc = "Remove 󰒤 mark" })
+      end, { desc = "Remove 󱡀 mark" })
 
       vim.keymap.set("n", "<leader>1", function()
         harpoon:list():select(1)
-      end, { desc = "󰒤 1" })
+      end, { desc = "󱡀 1" })
 
       vim.keymap.set("n", "<leader>2", function()
         harpoon:list():select(2)
-      end, { desc = "󰒤 2" })
+      end, { desc = "󱡀 2" })
 
       vim.keymap.set("n", "<leader>3", function()
         harpoon:list():select(3)
-      end, { desc = "󰒤 3" })
+      end, { desc = "󱡀 3" })
 
       vim.keymap.set("n", "<leader>4", function()
         harpoon:list():select(4)
-      end, { desc = "󰒤 4" })
+      end, { desc = "󱡀 4" })
+
+      vim.keymap.set("n", "<leader>5", function()
+        harpoon:list():select(5)
+      end, { desc = "󱡀 5" })
     end,
   },
 }
